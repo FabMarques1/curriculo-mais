802cfcc23b811e97e690613a32464c2f84b443e8
rayssa <lemesxd05@gmail.com>
2026-09-06 19:19:42 +0000

    editando css de login

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ if(isset($_SESSION['logado'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curriculo+</title>
-    <link rel="stylesheet" href="css/style.css?v=1">
     <link rel="stylesheet" href="css/index.css?v=1">
     <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
 </head>
--- login.php
+++ login.php
@@ -1,25 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+
     <title>CURRICULO+ | Login</title>
+
     <link rel="stylesheet" href="css/login.css">
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+    <link rel="shortcut icon"
+        href="assets/img/favicon.png"
+        type="image/x-icon">
 </head>
+
 <body>
-    <h3>Login</h3>
 
-    <form action="enviar-login.php" method="POST">
-        <label for="email">Digite seu e-mail</label>
-        <input name="email" id="email" type="email">
+    <main class="login-container">
+          <div class="logo">
+            CURRICULO<span>+</span>
+        </div>
 
-        <br>
+        <form action="enviar-login.php" method="POST">
+            <h3>Bem-vindo de volta!</h3>
+            <label for="email">
+                Digite seu e-mail
+            </label>
 
-        <label for="senha">Digite sua senha:</label>
-        <input name="senha" id="senha" type="password">
+            <input
+                name="email"
+                id="email"
+                type="email"
+                placeholder="seu@email.com"
+                autocomplete="email"
+                required
+            >
 
-        <button>Login</button>
-    </form>
+            <label for="senha">
+                Digite sua senha
+            </label>
+
+            <input
+                name="senha"
+                id="senha"
+                type="password"
+                placeholder="Digite sua senha"
+                autocomplete="current-password"
+                required
+            >
+            <button type="submit">
+                Login
+            </button>
+
+        </form>
+
+    </main>
+
 </body>
+
 </html>
