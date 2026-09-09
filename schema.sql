f22802d9714f9dd79237e631cd8f6856db7bc042
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-09-08 22:14:55 -0300

    feat: novas tabelas e relacionamentos
    Foi implementado novas tabelas para elaboração de uma atualização do site.

--- schema.sql
+++ schema.sql
@@ -1,34 +1,11 @@
+-- NÃO UTILIZAR, EM DESENVOLVIMENTO!!
+-- Fabricio.
+
 CREATE DATABASE curriculo_mais_db
 	DEFAULT CHARACTER SET utf8mb4
     DEFAULT COLLATE utf8mb4_0900_ai_ci;
     
 USE curriculo_mais_db;
-
-CREATE TABLE tbl_usuario(
-	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(40) NOT NULL,
-    sobrenome VARCHAR(75),
-    email VARCHAR(80) UNIQUE NOT NULL,
-    senha CHAR(64) NOT NULL,
-    cidade VARCHAR(30) NOT NULL DEFAULT 'Cruzeiro',
-    
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE tbl_telefone(
-	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    telefone CHAR(11) NOT NULL,
-    id_usuario SMALLINT UNSIGNED NOT NULL,
-    
-    PRIMARY KEY (id),
-    
-    CONSTRAINT fk_usuario 
-    FOREIGN KEY (id_usuario) 
-    REFERENCES tbl_usuario (id)
-    ON DELETE CASCADE
-);
-
--- ALTERAÇÕES
 
 CREATE TABLE IF NOT EXISTS tbl_cidade(
 	id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -45,26 +22,70 @@ INSERT INTO tbl_cidade (nome) VALUES
 ("Cachoeira Paulista"),
 ("Lorena");
 
-ALTER TABLE tbl_usuario
-MODIFY COLUMN cidade TINYINT UNSIGNED NOT NULL;
+CREATE TABLE IF NOT EXISTS tbl_usuario(
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(40) NOT NULL,
+    sobrenome VARCHAR(75),
+    email VARCHAR(80) UNIQUE NOT NULL,
+    senha CHAR(64) NOT NULL,
+	id_cidade TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    
+    PRIMARY KEY (id),
+    
+    CONSTRAINT fk_cidade
+    FOREIGN KEY (id_cidade)
+    REFERENCES tbl_cidade (id)
+);
 
-ALTER TABLE tbl_usuario
-RENAME COLUMN cidade TO id_cidade;
+CREATE TABLE IF NOT EXISTS tbl_telefone(
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    telefone CHAR(11) NOT NULL,
+    id_usuario SMALLINT UNSIGNED NOT NULL,
+    
+    PRIMARY KEY (id),
+    
+    CONSTRAINT fk_usuario 
+    FOREIGN KEY (id_usuario) 
+    REFERENCES tbl_usuario (id)
+    ON DELETE CASCADE
+);
 
-ALTER TABLE tbl_usuario
-ADD CONSTRAINT fk_cidade FOREIGN KEY (id_cidade) REFERENCES tbl_cidade (id);
+CREATE TABLE IF NOT EXISTS tbl_vaga(
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(75) NOT NULL,
+    descricao TEXT NOT NULL,
+    
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS tbl_curriculo(
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     resumo_profissional VARCHAR(200),
     curriculo CHAR(31) NOT NULL,
     data_envio TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_ativo BOOLEAN NOT NULL DEFAULT 1 CHECK (is_ativo BETWEEN 0 AND 1),
     id_usuario SMALLINT UNSIGNED NOT NULL,
+    id_vaga SMALLINT UNSIGNED NOT NULL,
     
     PRIMARY KEY (id),
     
     CONSTRAINT fk_usuario_curriculo
     FOREIGN KEY (id_usuario)
     REFERENCES tbl_usuario (id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
+    
+    CONSTRAINT fk_vaga_curriculo
+    FOREIGN KEY (id_vaga)
+    REFERENCES tbl_vaga (id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_instituicao(
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    id_cidade TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    
+    PRIMARY KEY (id)
 );
