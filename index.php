71d474a9d5fb7f188140d9c304fe5b5682c0d35b
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-09-10 15:26:26 -0300

    teste de deploy

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['logado'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curriculo+</title>
+    <title>teste de deploy</title>
     <link rel="stylesheet" href="css/index.css?v=1">
     <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
 </head>
