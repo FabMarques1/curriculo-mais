c187bdf1e9739e718d988e35b3fd2f0885588201
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-09-10 20:08:59 -0300

    refactor: ordem de execução

--- registro.php
+++ registro.php
@@ -17,7 +17,7 @@ $result = $stmt->get_result();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CURRÍCULO+ | Registro</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/registro.css">
+    <link rel="stylesheet" href="css/registro.css?v=1">
     <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
 </head>
 <body>
