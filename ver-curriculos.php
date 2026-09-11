739406e5e5c072f8283504c049620fa8ae6bc4be
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-09-10 22:06:52 -0300

    refactor: mudança no sql

--- ver-curriculos.php
+++ ver-curriculos.php
@@ -5,17 +5,62 @@ require_once('config/database.php');
 $result = null;
 $isDisabled = "disabled";
 
-if ($_SERVER['REQUEST_METHOD'] === "GET") {
+$vagas = "SELECT id, titulo FROM tbl_vaga";
+$stmt1 = $conn->prepare($vagas);
+$stmt1->execute();
+$resultVaga = $stmt1->get_result();
 
-    if (isset($_GET['filtro_vaga'])) {
-        $isDisabled = "";
+if (!empty($_GET) && $_SERVER['REQUEST_METHOD'] === "GET") {
 
-        $sql = "SELECT id, nome FROM tbl_instituicao";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->get_result();
+    $filtro_vaga = $_GET['filtro_vaga'];
+
+    if (isset($_GET['filtro_ordem']) && strtoupper($_GET['filtro_ordem']) === 'DESC') {
+        $filtro_ordem = "DESC";
+    } elseif(isset($_GET['filtro_ordem']) && strtoupper($_GET['filtro_ordem']) === 'ASC'){
+        $filtro_ordem = "ASC";
     }
+
+    $informacoes = "SELECT
+                    u.nome,
+                    u.sobrenome AS sobrenome,
+                    u.email,
+                    v.id,
+                    v.titulo,
+                    c.resumo_profissional,
+                    c.curriculo
+                FROM tbl_curriculo c
+                INNER JOIN tbl_usuario u
+                    ON c.id_usuario = u.id
+                INNER JOIN tbl_vaga v
+                    ON c.id_vaga = v.id
+                WHERE v.id = ?
+                ORDER BY nome " . $filtro_ordem;
+
+    $stmt3 = $conn->prepare($informacoes);
+    $stmt3->bind_param('i', $filtro_vaga);
+
 }
+
+if(empty($_GET)){
+    $informacoes = "SELECT
+                        u.nome,
+                        u.sobrenome,
+                        u.email,
+                        v.id,
+                        v.titulo,
+                        c.resumo_profissional,
+                        c.curriculo
+                    FROM tbl_curriculo c
+                    INNER JOIN tbl_usuario u
+                        ON c.id_usuario = u.id
+                    INNER JOIN tbl_vaga v
+                        ON c.id_vaga = v.id";
+
+    $stmt3 = $conn->prepare($informacoes);
+}
+
+$stmt3->execute();
+$resultInfo = $stmt3->get_result();
 
 ?>
 
@@ -29,25 +74,20 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
 <body>
     <h1>Currículos</h1>
     <form action="ver-curriculos.php" method="GET">
-        <label for="filtro_ordem">Ordem:</label>
+        <label for="filtro_ordem">Ordem por nome:</label>
         <select name="filtro_ordem" id="filtro_ordem">
             <option value="DESC">Decrescente</option>
             <option value="ASC">Crescente</option>
         </select>
 
-        <label for="filtro_instituicao">Instituição</label>
-        <select name="filtro_instituicao" id="filtro_instituicao" <?php echo $isDisabled; ?>>
-            <?php if ($result): ?>
-                <?php while($row = $result->fetch_assoc()): ?>
-                    <option value="<?php echo $row['id']; ?>"><?php echo htmlspecialchars($row['nome']); ?></option>
+        <label for="filtro_vaga">Filtrar por vaga:</label>
+        <select name="filtro_vaga" id="filtro_vaga">
+            <?php if ($resultVaga): ?>
+                <?php while($rowVaga = $resultVaga->fetch_assoc()): ?>
+                    <option value="<?php echo $rowVaga['id']; ?>"><?php echo htmlspecialchars($rowVaga['titulo']); ?></option>
                 <?php endwhile; ?>
+                <?php $stmt1->close(); ?>
             <?php endif; ?>
-        </select>
-
-        <label for="filtro_vaga">Filtrar por vaga <i>(se instituição for filtrada)</i>:</label>
-        <select name="filtro_vaga" id="filtro_vaga" <?php echo $isDisabled; ?>>
-            <option value="vaga1">Vaga 1</option>
-            <option value="vaga2">Vaga 2</option>
         </select>
 
         <button>Buscar</button>
@@ -62,14 +102,27 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
             <th>RESUMO PROFISSIONAL</th>
             <th>CURRÍCULO</th>
         </tr>
-        <tr>
-            <td>Fabricio</td>
-            <td>Henrique</td>
-            <td>fabricioteste@gmail.com</td>
-            <th>Marketing</th>
-            <th>Entusiasta por banco de dados, apaixonado por tecnologia.</th>
-            <th><a href="curriculos/">Acessar currículo</a></th>
-        </tr>
+        <?php if($resultInfo): ?>
+            <?php while($rowInfo = $resultInfo->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $rowInfo['nome']; ?></td>
+                    <td><?php echo $rowInfo['sobrenome']; ?></td>
+                    <td><?php echo $rowInfo['email']; ?></td>
+                    <td><?php echo $rowInfo['titulo']; ?></td>
+                    <td><?php echo $rowInfo['resumo_profissional']; ?></td>
+                    <td><a href="<?php echo $rowInfo['curriculo']; ?>">Acessar currículo</a></td>
+                </tr>
+            <?php endwhile; ?>
+            <?php $stmt3->close(); ?>
+        <?php else: ?>
+            <td>-------</td>
+            <td>-------</td>
+            <td>-------</td>
+            <td>-------</td>
+            <td>-------</td>
+            <td>-------</td>
+            <?php $stmt3->close(); ?>
+        <?php endif; ?>
     </table>
 </body>
 </html>
