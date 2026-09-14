c7e2dffd96c2ed5e64076110eca9f5ad2ad64e1f
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-09-14 18:55:08 -0300

    feat: forçar utf8mb4

--- config/database.php
+++ config/database.php
@@ -7,6 +7,7 @@ $database = "curriculo_mais_db";
 
 try{
     $conn = new MySQLi($host, $user, $password, $database);
+    $conn->set_charset("utf8mb4");
     # Conexão bem-sucedida!
 } catch (Exception $e) {
     die("Houve um problema na conexão com o banco de dados: " . $e->getMessage());
